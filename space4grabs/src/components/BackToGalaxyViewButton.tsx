0afca3ebeee381db00c114ed4688c9b990875f53
Ivan Hušnjak <ihusnja4@gmail.com>
2020-12-24 23:35:43 +0100

    Remove outstanding ts-ignore for missing types of bootstrap icons

--- space4grabs/src/components/BackToGalaxyViewButton.tsx
+++ space4grabs/src/components/BackToGalaxyViewButton.tsx
@@ -1,9 +1,7 @@
 import {Link} from "react-router-dom";
 import React from "react";
-// TODO remove ignore once this is resolved @link <https://github.com/ismamz/react-bootstrap-icons/issues/1>
-// @ts-ignore
-import * as Icons from "react-bootstrap-icons";
+import {ChevronLeft} from "react-bootstrap-icons";
 
 export const BackToGalaxyViewButton = () => (
-    <Link to="/" className="btn btn-secondary"><Icons.ChevronLeft/> Back to Galaxy View</Link>
+    <Link to="/" className="btn btn-secondary"><ChevronLeft/> Back to Galaxy View</Link>
 );
